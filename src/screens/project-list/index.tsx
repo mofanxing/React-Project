c6a6aa25bca3baae44b50328a28ef99d3f114328
张荣平 <1473166181@qq.com>
2022-02-10 15:57:23 +0800

    使用防抖

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { List } from './list'
 import { SearchPanel } from './search-panel'
 import { useEffect, useState } from 'react'
-import { cleanObject, useMount } from 'utils'
+import { cleanObject, useDebounce, useMount } from 'utils'
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -14,16 +14,17 @@ export const ProjectListScreen = () => {
   })
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
+  const debouncedParam = useDebounce(param, 2000)
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json())
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json())
       }
-    )
-  }, [param])
+    })
+  }, [debouncedParam])
 
   useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
