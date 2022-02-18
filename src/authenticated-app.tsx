a9c2b3c433656673c1596aa3278034c1cf6df0da
张荣平 <1473166181@qq.com>
2022-02-18 22:21:48 +0800

    添加路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -4,39 +4,54 @@ import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
 import styled from '@emotion/styled'
 import { Row } from 'components/lib'
 import { Button, Dropdown, Menu } from 'antd'
+import { Route, Routes } from 'react-router'
+import { ProjectScreen } from 'screens/project'
+import { BrowserRouter } from 'react-router-dom'
 export const AuthenticatedApp = () => {
-  const { logout, user } = useAuth()
   return (
     <Containdr>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
-          <h2>Logo</h2>
-          <h2>项目</h2>
-          <h2>用户</h2>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key={'logout'}>
-                  <Button type={'link'} onClick={logout}>
-                    登出
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button type={'link'} onClick={(e) => e.preventDefault()}>
-              Hi, {user?.name}
-            </Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
-        <ProjectListScreen></ProjectListScreen>
+        <BrowserRouter>
+          <Routes>
+            <Route path={'projects'} element={<ProjectListScreen />} />
+            <Route path={'projects/:projectId/*'} element={<ProjectScreen />} />
+            <Route index element={<ProjectListScreen />} />
+          </Routes>
+        </BrowserRouter>
       </Main>
     </Containdr>
+  )
+}
+
+const PageHeader = () => {
+  const { logout, user } = useAuth()
+  return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width={'18rem'} color={'rgb(38, 132, 255)'} />
+        <h2>Logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={'logout'}>
+                <Button type={'link'} onClick={logout}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type={'link'} onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
   )
 }
 
