ed0cfcf8b39949be06efb30ee7c9b886db13c78d
张荣平 <1473166181@qq.com>
2022-02-22 23:18:53 +0800

    路由完善

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,4 +1,5 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Route, Routes } from 'react-router'
 import { EpicScreen } from 'screens/Epic'
 import { KanbanScreen } from 'screens/Kanban'
 
@@ -6,11 +7,12 @@ export const ProjectScreen = () => {
   return (
     <div>
       <h1>ProjectScreen</h1>
-      <Link to={'/kanban'}>看板</Link>
-      <Link to={'/epic'}>任务组</Link>
+      <Link to={'kanban'}>看板</Link>
+      <Link to={'epic'}>任务组</Link>
       <Routes>
         <Route path={'/kanban'} element={<KanbanScreen />}></Route>
         <Route path={'/epic'} element={<EpicScreen />}></Route>
+        <Route index element={<KanbanScreen />}></Route>
       </Routes>
     </div>
   )
