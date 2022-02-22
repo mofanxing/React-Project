6de4e6e7bcb2e8b0def7bda56260d59d19265c4c
张荣平 <1473166181@qq.com>
2022-02-22 23:02:41 +0800

    新建看板,任务组

---
+++ src/screens/Epic/index.tsx
@@ -0,0 +1,3 @@
+export const EpicScreen = () => {
+  return <h1>2212</h1>
+}
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,3 +1,17 @@
+import { Link, Route, Routes } from 'react-router-dom'
+import { EpicScreen } from 'screens/Epic'
+import { KanbanScreen } from 'screens/Kanban'
+
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={'/kanban'}>看板</Link>
+      <Link to={'/epic'}>任务组</Link>
+      <Routes>
+        <Route path={'/kanban'} element={<KanbanScreen />}></Route>
+        <Route path={'/epic'} element={<EpicScreen />}></Route>
+      </Routes>
+    </div>
+  )
 }
