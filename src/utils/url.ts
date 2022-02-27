59162b0473b5e51726709965c80dab340ecf9401
张荣平 <1473166181@qq.com>
2022-02-27 22:31:31 +0800

    fix

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -4,7 +4,6 @@ import { useSearchParams } from 'react-router-dom'
  * 返回页面url中，指定键的参数值
  */
 
-//fix
 export const useUrlQueryParam = (keys: string[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
   return [
