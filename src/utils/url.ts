df93e6b811fcd0c18983b9727d3b0b0b34bea3d0
张荣平 <1473166181@qq.com>
2022-03-01 23:15:19 +0800

    url hooks

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -7,9 +7,9 @@ import { useSearchParams } from 'react-router-dom'
 export const useUrlQueryParam = (keys: string[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
   return [
-    keys.reduce((prev: {}, key: string) => {
-      return { ...prev, [key]: searchParams.get(key) }
-    }, {}),
+    keys.reduce((prev: { [p: string]: string }, key: string) => {
+      return { ...prev, [key]: searchParams.get(key) || '' }
+    }, {} as { [key in string]: string }),
     setSearchParams,
   ] as const
 }
