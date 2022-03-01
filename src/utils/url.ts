8898c1580dfc6f0f60b9e85615679f87ba90aae1
张荣平 <1473166181@qq.com>
2022-03-01 23:38:59 +0800

    注释问题

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,7 +3,7 @@ import { useSearchParams } from 'react-router-dom'
 /**
  * 返回页面url中，指定键的参数值
  */
-
+//此处类型有问题
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
   return [
