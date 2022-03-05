81ff11642352240dea5b6f1c97208139d72e7748
张荣平 <1473166181@qq.com>
2022-03-05 16:33:58 +0800

    fix

--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -35,7 +35,7 @@ export const SearchPanel = ({ users, param, setParam }: SearchPanelTypes) => {
         >
           <Select.Option value={''}>负责人</Select.Option>
           {users.map((user) => (
-            <Select.Option key={user.id} value={user.id}>
+            <Select.Option key={user.id} value={String(user.id)}>
               {user.name}
             </Select.Option>
           ))}
