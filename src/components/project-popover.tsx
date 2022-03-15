f3274913d001db5d91b425753aca85cb29ac3d44
张荣平 <1473166181@qq.com>
2022-03-15 22:44:03 +0800

    popover组件开发

--- src/components/project-popover.tsx
+++ src/components/project-popover.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import { List, Popover, Typography } from 'antd'
 import { useProjects } from 'utils/project'
 
@@ -6,16 +7,16 @@ export const ProjectPopover = () => {
   const pinnedProjects = projects?.filter((project) => project.pin)
 
   const content = (
-    <div>
+    <ContentContainer>
       <Typography.Text type={'secondary'}>收藏项目</Typography.Text>
       <List>
         {pinnedProjects?.map((project) => (
-          <List.Item>
+          <List.Item key={project.id}>
             <List.Item.Meta title={project.name}></List.Item.Meta>
           </List.Item>
         ))}
       </List>
-    </div>
+    </ContentContainer>
   )
   return (
     <Popover placement={'bottom'} content={content}>
@@ -23,3 +24,7 @@ export const ProjectPopover = () => {
     </Popover>
   )
 }
+
+const ContentContainer = styled.div`
+  min-width: 30rem;
+`
