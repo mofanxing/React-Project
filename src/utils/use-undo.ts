ac21827480a4cf613e6be5f36822ac06f6ee73e1
张荣平 <1473166181@qq.com>
2022-03-20 22:34:58 +0800

    undo 完成

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -13,5 +13,39 @@ export const useUndo = <T>(initialPresent: T) => {
 
     const previous = past[past.length - 1]
     const newPast = past.slice(0, past.length - 1)
+
+    setPast(newPast)
+    setPresent(previous)
+    setFuture([present, ...future])
   }
+
+  const redo = () => {
+    if (!canRedo) return
+
+    const next = future[0]
+    const newFuture = future.slice(1)
+
+    setPast([...past, present])
+    setPresent(next)
+    setFuture(newFuture)
+  }
+
+  const set = (newPresent: T) => {
+    if (newPresent === present) return
+
+    setPast([...past, present])
+    setPresent(newPresent)
+    setFuture([])
+  }
+
+  const reset = (newPresent: T) => {
+    setPast([])
+    setPresent(newPresent)
+    setFuture([])
+  }
+
+  return [
+    { past, present, future },
+    { set, reset, undo, redo, canUndo, canRedo },
+  ]
 }
