5f41cd4e097ee632855a7536804e74853649c6d0
张荣平 <1473166181@qq.com>
2022-06-21 21:20:17 +0800

    fix

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,24 +1,10 @@
-import { useCallback, useEffect } from 'react'
 import { Project } from 'screens/project-list/list'
-import { cleanObject } from 'utils'
+
 import { useAsync } from 'utils/use-async'
 import { useHttp } from 'utils/http'
 
 export const useProjects = (param?: Partial<Project>) => {
   const client = useHttp()
-  const { run, ...result } = useAsync<Project[]>()
-
-  const fetchProjects = useCallback(
-    () => client('projects', { data: cleanObject(param || {}) }),
-    [client, param]
-  )
-  // 使用useCallback解决加入依赖无限渲染问题
-  useEffect(() => {
-    run(fetchProjects(), {
-      retry: fetchProjects,
-    })
-  }, [param, run, fetchProjects])
-  return result
 }
 
 export const useEditProject = () => {
