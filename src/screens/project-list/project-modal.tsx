676e6a746b284cf33d3ba417cb06397c6e53d2f8
张荣平 <1473166181@qq.com>
2022-06-27 13:05:14 +0800

    fix

--- src/screens/project-list/project-modal.tsx
+++ src/screens/project-list/project-modal.tsx
@@ -1,11 +1,29 @@
 import { Button, Drawer, Form, Input, Spin } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
+import { ErrorBox } from 'components/lib'
+import { UserSelect } from 'components/user-select'
+import { useEffect } from 'react'
+import { useAdditProject, useEditProject } from 'utils/project'
 import { useProjectModal } from './util'
 
 export const ProjectModal = () => {
   const { projectModalOpen, close, editingProject, isLoading } =
     useProjectModal()
   const title = editingProject ? '编辑项目' : '创建项目'
-  const onFinish = () => {}
+  const useMutateProject = editingProject ? useEditProject : useAdditProject
+  const { mutateAsync, error, isLoading: mutateLoading } = useMutateProject()
+
+  const [form] = useForm()
+  const onFinish = (values: any) => {
+    mutateAsync({ ...editingProject, ...values }).then(() => {
+      form.resetFields()
+      close()
+    })
+  }
+
+  useEffect(() => {
+    form.setFieldsValue(editingProject)
+  }, [editingProject, form])
   return (
     <Drawer onClose={close} visible={projectModalOpen} width={'100%'}>
       {isLoading ? (
@@ -13,9 +31,11 @@ export const ProjectModal = () => {
       ) : (
         <>
           <h1>{title}</h1>
+          <ErrorBox error={error}></ErrorBox>
           <Form
             layout={'vertical'}
             style={{ width: '40rem' }}
+            form={form}
             onFinish={onFinish}
           >
             <Form.Item
@@ -24,6 +44,25 @@ export const ProjectModal = () => {
               rules={[{ required: true, message: '请输入项目名称' }]}
             >
               <Input placeholder={'请输入项目名称'}></Input>
+            </Form.Item>
+            <Form.Item
+              label={'部门'}
+              name={'organization'}
+              rules={[{ required: true, message: '请输入部门名称' }]}
+            >
+              <Input placeholder={'请输入部门名称'}></Input>
+            </Form.Item>
+            <Form.Item label={'负责人'} name={'personId'}>
+              <UserSelect defaultOptionName={'负责人'}></UserSelect>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                loading={mutateLoading}
+                type={'primary'}
+                htmlType={'submit'}
+              >
+                提交
+              </Button>
             </Form.Item>
           </Form>
         </>
