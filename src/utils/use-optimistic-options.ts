08e85ee43ed55b39bfc9f1ed719c25d3013672f3
张荣平 <1473166181@qq.com>
2022-06-30 17:21:28 +0800

    fix

--- src/utils/use-optimistic-options.ts
+++ src/utils/use-optimistic-options.ts
@@ -1,1 +1,22 @@
-export const useConfig = () => {}
+import { QueryKey, useQueryClient } from 'react-query'
+
+export const useConfig = (
+  queryKey: QueryKey,
+  callback: (target: any, old?: any[]) => any[]
+) => {
+  const queryClient = useQueryClient()
+
+  return {
+    onSuccess: () => queryClient.invalidateQueries(queryKey),
+    async onMutate(target: any) {
+      const previousItems = queryClient.getQueryData(queryKey)
+      queryClient.setQueryData(queryKey, (old?: any[]) => {
+        return callback(target, old)
+      })
+      return { previousItems }
+    },
+    onError(error: any, newItem: any, context: any) {
+      queryClient.setQueryData(queryKey, context.previousItems)
+    },
+  }
+}
